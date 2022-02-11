7d84d20d928d191d03a656238addcde5f560fc85
Yujoong Kim <kimyujoong@gmail.com>
2022-02-11 15:43:21 -0500

    new

---
+++ vue/src/views/League.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="leagues">
+    <create-league />
+
+    <display-league/>
+  </div>
+</template>
+
+<script>
+import CreateLeague from "../components/CreateLeague.vue";
+// import DisplayAllLeague from "../components/DisplayLeague"
+import DisplayLeague from '../components/DisplayLeague.vue';
+export default {
+  components: {  CreateLeague, DisplayLeague },
+};
+</script>
+
+<style>
+#leagues {
+  background-color: #f7ecde;
+  padding: 20px;
+  border-radius: 10px;
+  margin: 0 auto;
+  display: flex;
+  height: 50%;
+  width: 50%;
+  justify-content: space-between;
+}
+
+</style>
---
+++ vue/src/views/Match.vue
@@ -0,0 +1,52 @@
+<template>
+<div class="match">
+    <div id="previous-matches-div">
+        <previous-matches />
+    </div>
+
+    <div id="match-scores-div">
+    <create-match />
+    <add-match-score/>
+    </div>
+
+    <div id="upcoming-matches-div">
+        <upcoming-matches />
+    </div>
+
+</div>
+</template>
+
+<script>
+import AddMatchScore from '../components/AddMatchScore.vue';
+import CreateMatch from '../components/CreateMatch.vue';
+import PreviousMatches from '../components/PreviousMatches.vue';
+import UpcomingMatches from '../components/UpcomingMatches.vue';
+
+
+export default {
+    components: { CreateMatch, AddMatchScore, PreviousMatches, UpcomingMatches },
+    name: "match",
+
+}
+</script>
+
+<style scoped>
+.match {
+    width: 100vw;
+    height: 80vh;
+    display: flex;
+    justify-content: space-between;
+    padding: 20px;
+    align-content: center;
+}
+
+#previous-matches-div,
+#upcoming-matches-div {
+    width: 20vw;
+    height: 60vh;
+}
+
+
+
+
+</style>>
